dac6a0ff22b0b87b04dcfb9c44756366c229eb15
caudilci <eP757x9+>
2021-01-09 00:43:29 -0800

    implemented enemy pathing

--- Objects/Enemy.gd
+++ Objects/Enemy.gd
@@ -11,6 +11,61 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# line of sight algorithm found here: http://www.roguebasin.com/index.php?title=Bresenham%27s_Line_Algorithm
+func hasLineOfSight(start_coord, end_coord, tilemap: TileMap):
+	var x1 = start_coord[0]
+	var y1 = start_coord[1]
+	var x2 = end_coord[0]
+	var y2 = end_coord[1]
+	var dx = x2 - x1
+	var dy = y2 - y1
+	# Determine how steep the line is
+	var is_steep = abs(dy) > abs(dx)
+	var tmp = 0
+	# Rotate line
+	if is_steep:
+		tmp = x1
+		x1 = y1
+		y1 = tmp
+		tmp = x2
+		x2 = y2
+		y2 = tmp
+	# Swap start and end points if necessary and store swap state
+	var swapped = false
+	if x1 > x2:
+		tmp = x1
+		x1 = x2
+		x2 = tmp
+		tmp = y1
+		y1 = y2
+		y2 = tmp
+		swapped = true
+	# Recalculate differentials
+	dx = x2 - x1
+	dy = y2 - y1
+	
+	# Calculate error
+	var error = int(dx / 2.0)
+	var ystep = 1 if y1 < y2 else -1
+
+	# Iterate over bounding box generating points between start and end
+	var y = y1
+	var points = []
+	for x in range(x1, x2 + 1):
+		var coord = [y, x] if is_steep else [x, y]
+		points.append(coord)
+		error -= abs(dy)
+		if error < 0:
+			y += ystep
+			error += dx
+	
+	if swapped:
+		points.invert()
+	
+	for point in points:
+		if tilemap.get_cell(point[0], point[1]) >= 0:
+			return false
+	return true
+
+func getGridPath(startCoord, endCoord, astar: AStar2D, astarPointsCache: Dictionary):
+	return astar.get_point_path(astarPointsCache[str(startCoord)], astarPointsCache[str(endCoord)])
